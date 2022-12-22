b208e7e800ccedc69086ecbfd726de777ddec095
최승민 <seungmin.choi@linefriends.com>
2022-12-22 16:49:27 +0900

    [ Login, Register ] Login, Register UI Complete
    - 로그인, 계정 생성 화면 UI 완성

--- Messenger/Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Messenger/Controllers/ConversationsViewController.swift
@@ -11,7 +11,22 @@ class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .red
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let logged_in = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !logged_in {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+    
+    
 }
 
